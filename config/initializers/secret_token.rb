a76cc3b33bbae2e9fbc0ff900890aee73ec2f776
Jesus Figueroa <jesus.figueroa@crowdint.com>
2014-06-11 10:53:12 -0500

    Add new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MagmarailsCom::Application.config.secret_key_base = 'b757edf5f2477b81c677349c4dde1db0c7765ac35c37108a02efb597238e1ed5107cdf82722e92efdec5b5468aef8f1962328d3a8492f967e79782c080508647'
