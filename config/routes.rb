e3a11f2c13f458274cf411d180912aa0f16bd54a
Gilberto Villa <ingilniero@gmail.com>
2014-06-23 11:57:01 -0500

    Add logic for newsletter subscriptions

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ MagmarailsCom::Application.routes.draw do
   get '/*id', to: 'site2014#show', format: false, constraints: { id: /2014\/.+/ }
   get '/2014', to: 'site2014#show', id: '2014/home'
   post '/send_email', to: 'site2014#send_email'
+  post '/newsletter', to: 'newsletter_subscriptions#create'
 end
