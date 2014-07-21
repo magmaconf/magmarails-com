423dd4e8ce8516d13de9042b7fd0a19eabcb7c55
Gilberto Villa <ingilniero@gmail.com>
2014-07-21 10:43:49 -0500

    Update flickraw adapter to retrieve photos from a photoset

--- app/models/flickr_adapter.rb
+++ app/models/flickr_adapter.rb
@@ -13,9 +13,10 @@ end
 
 class FlickrAdapter
   def initialize
-    @user_id = '125306266@N02'
+    @user_id = '63319310@N04'
     @api_key = FlickRaw.api_key
     @secret = FlickRaw.shared_secret
+    @photoset = '72157645528624066'
   end
 
   def get_photos(page = 1, per_page = 10)
@@ -23,28 +24,52 @@ class FlickrAdapter
 
     public_photos.each_with_object([]) do |photo, photos|
       photos << FlickrPhoto.new({
+        id: photo.id,
         title: photo.title,
         description: photo.description,
         url: photo.url_l,
         thumbnail: photo.url_m,
-        type: photo_type(photo.tags)
+        type: photo_type(photo)
       })
     end
 
   end
 
-  def photo_type(tags)
-    tags.include?('konstantin') ? 'konstantin_photo' : 'photo'
+  def photos_left?(page, per_page)
+    (page.to_i * per_page.to_i) < total_photos
+  end
+
+  def total_photos
+    @total_photos ||= flickr.photosets.getInfo(photoset_id: @photoset).photos
+  end
+
+  def photo_type(photo)
+    konstantin_photo?(photo)  ? 'konstantin_photo' : 'photo'
+  end
+
+  def konstantin_photo?(photo)
+    photo.tags.include?('konstantin') || (photo.description =~ /Photo by Konstantin Hasse/)
   end
 
   def get_user_photos(page, per_page)
-    flickr.people.getPublicPhotos(
-        {
-          api_key: @api_key,
-          user_id: @user_id,
-          page: page,
-          per_page: per_page,
-          extras: 'description, url_m, url_l, tags'
-        })
+    photoset = []
+
+    if photos_left?(page, per_page)
+
+      photoset = flickr.photosets.getPhotos(
+          {
+            api_key: @api_key,
+            user_id: @user_id,
+            photoset_id: @photoset,
+            page: page,
+            per_page: per_page,
+            extras: 'description, url_m, url_l, tags'
+          })
+
+      @total_photos = photoset.total
+      photoset = photoset.photo
+    end
+
+   photoset
   end
 end
