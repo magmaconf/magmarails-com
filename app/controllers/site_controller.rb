faacc6aa521acb73f664bab083d83730e6e333d4
Adrian Castillo <rodacato@gmail.com>
2015-02-18 00:05:03 -0600

    Add broad style structure

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,7 +1,7 @@
-class Site2014Controller < ApplicationController
+class SiteController < ApplicationController
   include HighVoltage::StaticPage
 
-  layout '2014/application'
+  layout "2015/application"
 
   def send_email
     TransactionMailer.sponsor_mail(params).deliver
