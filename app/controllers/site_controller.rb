731ca679df009d1fda4b55c72b2ea5b0e3e400c2
Mumo Carlos <mumo.crls@gmail.com>
2015-03-18 12:17:12 -0600

    Add logic to select layout's version

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,10 +1,19 @@
 class SiteController < ApplicationController
   include HighVoltage::StaticPage
 
-  layout "2015/application"
+  layout :set_layout
 
   def send_email
     TransactionMailer.sponsor_mail(params).deliver
     render nothing: true
+  end
+
+  private
+  def set_layout
+    is2014? && '2014/application' || '2015/application'
+  end
+
+  def is2014?
+    params[:id].match(/^2014/)
   end
 end
