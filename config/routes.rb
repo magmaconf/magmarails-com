1182a25991d90e97d57a18739ef24295ea0535bf
Mumo Carlos <mumo.crls@gmail.com>
2015-03-18 12:16:38 -0600

    Set root to site#show

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 MagmarailsCom::Application.routes.draw do
-  root to: 'high_voltage/pages#show', id: 'home'
-
   get '/*id', to: 'site#show', format: false, constraints: { id: /2014\/.+/ }
   get '/2014', to: 'site#show', id: '2014/home'
 
   get '/gallery_photos', to: 'gallery#show'
   post '/send_email', to: 'site#send_email'
   post '/newsletter', to: 'newsletter_subscriptions#create'
+
+  root to: 'site#show', id: 'home'
 end
