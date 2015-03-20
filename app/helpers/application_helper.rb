d57de39ecf2b9642909d46e92d84d83822b65fc3
Omar Garcia <isc.omargarcia@gmail.com>
2015-03-20 12:37:39 -0600

    Renamed main menu files

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
 	def main_menu
-		isRoot? && 'pages/2015/partials/root_main_menu' || 'pages/2015/partials/main_menu'
+		isRoot? && 'pages/2015/partials/main_menu' || 'pages/2015/partials/pages_main_menu'
 	end
 
 	def isRoot?
