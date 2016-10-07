0e7245541bcd05459e6ef34b2b48e340ac194d2e
Jose Trinidad Espinoza Gonzalez <trinoeg8@gmail.com>
2016-10-07 09:36:54 -0500

    - Set route to speakers - Add :year param to root path

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ MagmarailsCom::Application.routes.draw do
   get  '/gallery_photos', to: 'gallery#show'
   post '/send_email',     to: 'site#send_email'
   post '/newsletter',     to: 'newsletter_subscriptions#create'
-  get  '/2014',           to: 'site#show', id: '2014/home'
-
-  get '/*id',            to: 'site#show', format: false
+  get '/:year/speakers',  to: 'site#speakers'
+  get '/:year/*id',       to: 'site#show', format: false
   root to: 'site#show', id: 'home'
+  get '/*id',             to: 'site#show'
 end
 
