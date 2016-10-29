b9bbecd04f80eda7c70a316fc369f6a8b06bff2a
Jose Trinidad Espinoza Gonzalez <trinoeg8@gmail.com>
2016-10-28 19:17:22 -0500

    Finish Speakers design for CFP 2017

--- app/assets/javascripts/2017/modules/speakers.js.coffee
+++ app/assets/javascripts/2017/modules/speakers.js.coffee
@@ -10,10 +10,16 @@ $ ->
       $(this).find('p').hide()
 
 
-  $('.more').on 'click', (event) ->
-    speakerBox = $($(event.target).parents('.speaker-box')[0] || $(event.target).parents('.speaker-box-large')[0])
+  $('.more').on 'click', ->
+    speakerBox = $($(event.target).parents('.speaker-box')[0] or $(event.target).parents('.speaker-box-large')[0])
     if !speakerBox.hasClass('flipped')
-      $('.flipped').removeClass('flipped')
-    speakerBox.toggleClass('flipped')
+      $('.flipped').removeClass 'flipped'
+    speakerBox.toggleClass 'flipped'
 
+  $('.speaker-box').on 'click', ->
+    if !$(this).hasClass('flipped')
+      $('.flipped').removeClass 'flipped'
+    $(this).toggleClass 'flipped'
 
+  $('.close-icon').on 'click', (event) ->
+    $(this).parent().parent().parent().removeClass('flipped');
--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -12,9 +12,12 @@ class SiteController < ApplicationController
 
   def speakers
     @id = 'speakers'
-    @speakers_response = @magmaconf_service.get_speakers
-    if is_response_ok? @speakers_response
-      @speakers = JSON.parse(@speakers_response[:data])['speakers']
+    speakers_response = @magmaconf_service.get_speakers
+    if is_response_ok? speakers_response
+      @speakers_data = {
+        keynotes: get_keynotes(speakers_response),
+        speakers: get_speakers(speakers_response)
+      }
     else
       @speakers = {}
     end
@@ -51,6 +54,18 @@ class SiteController < ApplicationController
 
   def is_response_ok? response
     response[:status] == MagmaconfApiService::HTTP_SUCCESS_CODE
+  end
+
+  def get_keynotes speakers_response
+    JSON.parse(speakers_response[:data]).fetch('speakers', {}).keep_if do |speaker|
+      speaker["is_keynote"]
+    end
+  end
+
+  def get_speakers speakers_response
+    JSON.parse(speakers_response[:data]).fetch('speakers', {}).keep_if do |speaker|
+      !speaker["is_keynote"]
+    end
   end
 
   def set_events events_response
