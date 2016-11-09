3d5f916d6eea9c0dce553a8d2cb58e583216f7e3
Jose Trinidad Espinoza Gonzalez <trinoeg8@gmail.com>
2016-11-09 10:00:50 -0600

    Fix typo in 'Getting to Manzanillo' section [#133551347]

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,22 @@ module ApplicationHelper
     html_options[:class] = "#{html_options[:class]} #{active_class}" if current_page?(options)
     link_to(name, options, html_options, &block)
   end
+
+  def get_cfp_partial_view id
+    case id
+      when 'sponsors'
+        "pages/2017/partials/asides/sponsors_info"
+      else
+        "pages/2017/partials/welcome"
+    end
+  end
+
+  def render_reylero_img id
+    case id
+      when 'sponsors'
+        image_tag('2017/reylero_beach.svg', class: 'reylero-beach')
+      else
+        image_tag('2017/reylero_beach.svg', class: 'reylero-beach')
+    end
+  end
 end
